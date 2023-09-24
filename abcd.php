0c09abb250e2e1ca8f822a6cf63193a7096a996f
AryashBajaj <aryashbajaj@gmail.com>
2023-09-24 13:08:55 +0530

    commit

--- abcd.php
+++ abcd.php
@@ -2,9 +2,9 @@
 $conn = mysqli_connect("localhost", "root", "", "student_dropout");
 $sql = "SELECT * FROM school";
 $result = mysqli_query($conn, $sql);
-if ($result) {
-    echo "Succesful";
-} else {
-    echo "Unsuccesful";
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
 }
+echo json_encode($data);
 ?>
