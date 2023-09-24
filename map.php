b99395e767037c195cc8fc9417765079311cb667
AryashBajaj <aryashbajaj@gmail.com>
2023-09-25 02:01:36 +0530

    commit

--- map.php
+++ map.php
@@ -13,58 +13,57 @@
         console.log("Hello");
         var js;
         var xml = new XMLHttpRequest();
-        xml.open("GET", "abcd.php", true);
+        xml.open("GET", "fetchRate.php", true);
         xml.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText);
-                var js = JSON.parse(this.responseText);
+                var js= JSON.parse(this.responseText);
                 console.log(js);
-                var data = 100*(js[0].DS/js[0].TS);
-                drawDropoutMap(data);
+                console.log(typeof(Number(js[0].Rate)));
+                drawDropoutMap(js);
             }
         }
         xml.send();
     }
-    function drawDropoutMap(data) {
+    function drawDropoutMap(js) {
         console.log("Draw function");
         var dropoutData = google.visualization.arrayToDataTable([
             ['State Code', 'State', 'Dropout Rates per state'],
-            ['IN-UP','Uttar Pradesh', 25],
-            ['IN-MH','Maharashtra', 32],
-            ['IN-BR','Bihar', 31],
-            ['IN-WB','West Bengal', 32],
-            ['IN-MP','Madhya Pradesh', data],
-            ['IN-TN','Tamil Nadu', 33],
-            ['IN-RJ','Rajasthan', 33],
-            ['IN-KA','Karnataka', 29],
-            ['IN-GJ','Gujarat', 34],
-            ['IN-AP','Andhra Pradesh', 32],
-            ['IN-OR','Orissa', 33],
-            ['IN-TG','Telangana', 33],
-            ['IN-KL','Kerala', 31],
-            ['IN-JH','Jharkhand', 29],
-            ['IN-AS','Assam', 28],
-            ['IN-PB','Punjab', 30],
-            ['IN-CT','Chhattisgarh', 33],
-            ['IN-HR','Haryana', 30],
-            ['IN-JK','Jammu and Kashmir', 20],
-            ['IN-UT','Uttarakhand', 28],
-            ['IN-HP','Himachal Pradesh', 17],
-            ['IN-TR','Tripura', 31],
-            ['IN-ML','Meghalaya', 21],
-            ['IN-MN','Manipur', 22],
-            ['IN-NL','Nagaland', 22],
-            ['IN-GA','Goa', 32],
-            ['IN-AR', 'Arunachal Pradesh', 33],
-            ['IN-MZ','Mizoram', 23],
-            ['IN-SK','Sikkim', 24],
-            ['IN-DL','Delhi', 31],
-            ['IN-PY','Puducherry', 33],
-            ['IN-CH','Chandigarh', 30],
-            ['IN-AN','Andaman and Nicobar Islands', 30],
-            ['IN-DN','Dadra and Nagar Haveli', 30],
-            ['IN-DD','Daman and Diu', 29],
-            ['IN-LD','Lakshadweep', 31]
+            ['IN-UP','Uttar Pradesh', Number(js[34].Rate)],
+            ['IN-MH','Maharashtra', Number(js[21].Rate)],
+            ['IN-BR','Bihar', Number(js[4].Rate)],
+            ['IN-WB','West Bengal', Number(js[36].Rate)],
+            ['IN-MP','Madhya Pradesh', Number(js[20].Rate)],
+            ['IN-TN','Tamil Nadu', Number(js[31].Rate)],
+            ['IN-RJ','Rajasthan', Number(js[29].Rate)],
+            ['IN-KA','Karnataka', Number(js[16].Rate)],
+            ['IN-GJ','Gujarat', Number(js[11].Rate)],
+            ['IN-AP','Andhra Pradesh', Number(js[1].Rate)],
+            ['IN-OR','Orissa', Number(js[26].Rate)],
+            ['IN-TG','Telangana', Number(js[32].Rate)],
+            ['IN-KL','Kerala', Number(js[17].Rate)],
+            ['IN-JH','Jharkhand', Number(js[15].Rate)],
+            ['IN-AS','Assam', Number(js[3].Rate)],
+            ['IN-PB','Punjab', Number(js[28].Rate)],
+            ['IN-CT','Chhattisgarh', Number(js[6].Rate)],
+            ['IN-HR','Haryana', Number(js[12].Rate)],
+            ['IN-JK','Jammu and Kashmir', Number(js[14].Rate)],
+            ['IN-UT','Uttarakhand', Number(js[35].Rate)],
+            ['IN-HP','Himachal Pradesh', Number(js[13].Rate)],
+            ['IN-TR','Tripura', Number(js[33].Rate)],
+            ['IN-ML','Meghalaya', Number(js[23].Rate)],
+            ['IN-MN','Manipur', Number(js[22].Rate)],
+            ['IN-NL','Nagaland', Number(js[25].Rate)],
+            ['IN-GA','Goa', Number(js[10].Rate)],
+            ['IN-AR','Arunachal Pradesh', Number(js[2].Rate)],
+            ['IN-MZ','Mizoram', Number(js[24].Rate)],
+            ['IN-SK','Sikkim', Number(js[30].Rate)],
+            ['IN-DL','Delhi', Number(js[9].Rate)],
+            ['IN-PY','Puducherry', Number(js[27].Rate)],
+            ['IN-CH','Chandigarh', Number(js[5].Rate)],
+            ['IN-AN','Andaman and Nicobar Islands', Number(js[0].Rate)],
+            ['IN-DN','Dadra and Nagar Haveli', Number(js[7].Rate)],
+            ['IN-DD','Daman and Diu', Number(js[8].Rate)],
+            ['IN-LD','Lakshadweep', Number(js[19].Rate)]
 
         ]);
 
